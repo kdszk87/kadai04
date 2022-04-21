9ffae50fc73b2c3c0bf1cd262e342d1d2e966e4f
kdszk87 <kodai.suzuki.cd@gmail.com>
2022-04-22 05:02:52 +0900

    力尽きます、、

--- js/input.js
+++ js/input.js
@@ -1,83 +1,71 @@
 // やりたいこと
-// ローカルストレージにデータがないときは「no data」的な文を表示する
-// すでに保存されている鍵と同じ名前で保存しようとしたらエラーが表示される
-// 消したいものを選んで消せるようにする
-// 消す前に「本当に削除しますか？」のダイアログ？ポップアップ？が出るようにする
-// 入力フォームに文字がないときはセーブを押せないようにする/両方に何かしらの入力があるときセーブが押せるようにする
-
-// 入力ページのローカルストレージデータをオブジェクトにして、JSONに変換して、セーブデータ一覧ページでgetJSONで持ってこれないか(GLS JS-03-03 getJSON関数より)
+// 1 ローカルストレージにデータがないときは「no data」的な文を表示する
+// 2 すでに保存されている鍵と同じ名前で保存しようとしたらエラーが表示される
+// 3 消したいものを選んで消せるようにする
+// 4 消す前に「本当に削除しますか？」のダイアログ？ポップアップ？が出るようにする
+// 5 入力フォームに文字がないときはセーブを押せないようにする/両方に何かしらの入力があるときセーブが押せるようにする
+// 6 入力ページのローカルストレージデータをオブジェクトにして、JSONに変換して、セーブデータ一覧ページでgetJSONで持ってこれないか(GLS JS-03-03 getJSON関数より)
 
 
 //1.Save クリックイベント
+//両方のフォームに文字があるときのみ押せるようにする
+$(function(){
+    $("#save").prop("disabled", true); // 初期状態のボタンは無効
+    $("#key").on("input", function() {
+        const input = $("#key").val(); //input に入力された文字を取得
+        // console.log(input);
+        if(input){ //もし文字が入っていれば
+            $("#save").prop('disabled', false); 
+            //disabled を無効にする＝ボタンが押せる
+        }else{
+            $("#save").prop('disabled', true); 
+            //disabled を有効にする＝ボタンが押せない
+        }
+    });
+});
+
 $("#save").on("click", function (){
+
+    let label = `<p>Saved the followed Record</p>`
+    $("#explain").append(label);
+
+
     let key = $("#key").val();
     // valでkeyに入力された中身を持ってきている
     console.log(key);
     let flow = $("#flow").val();
     // valでflowに入力された中身を持ってきている
     console.log(flow);
-
     // ↓local storageという保存場所にデータを保存する
     localStorage.setItem(key, flow);
 
-    // 下はテンプレートリテラル
-    // 教材の書き方を楽にしたもの
-    // 斜めの「`」はShift + ＠
     let html = `
     <tr>
-        <td><input type="checkbox" /></td>
+        <p>Save Record</p>
         <th>${key}</th>
         <td>${flow}</td>
-        </tr>
+    </tr>
     `
-    // 画面に表示
+    // #list内に変数html内の情報を表示
     $("#list").append(html);
-    // メモパッドの下に変数html内の情報を表示
+
+    //Saveしたら入力フォームをクリアする
+    $('input').val("");
+    $('textarea').val("");
+
 })
-
-
-//2.clear クリックイベント
-$("#clear").on("click",function(){
-    // alert("clear");
-    localStorage.clear();
-    // ローカルストレージを空にする
-    $("#list").empty();
-    // listに表示されているhtml(メモパッドの下)を削除する
-    // →ローカルストレージに保存されているものを消すこととhtmlに表示されているものを消すことは別物
-});
-
-
-//3.ページ読み込み：保存データ取得表示
-// ページのリロードをしたときにローカルストレージにデータがあったら表示する
-// 繰り返し処理と配列を組み合わせて、登録されているデータがあったら表示する
-for(let i=0; i < localStorage.length; i++) { //←なんかエラー出てる「Assignment to constant variable」←constをletにしたら直った。constは再代入、再宣言ができないのにkeyを上でもここでも宣言していたからエラーが出ていた模様。
-    let key = localStorage.key(i); 
-    let value = localStorage.getItem(key);
-    // 下はテンプレートリテラル
-    // 斜めの「`」はShift + ＠
-        let html = `
-        <tr> 
-            <td><input type="checkbox" /></td>
-            <th>${key}</th>
-            <td>${value}</td>
-        </tr>
-        `
-        // 画面に表示
-        $("#list").append(html);
-        // #listの先頭に変数htmlを追加
-    }
-    // alert("for");
+    
 
     for(let i=0; i < localStorage.length; i++) {
     // ローカルストレージ内のデータをオブジェクトにしたい
     let data = localStorage.key(i);
     //オブジェクトにしたものをJSONに変換したい
-    let data_JSON = JSON.parse(data);
-    console.log(data_JSON);
-    // let data = localStorage.getItem(key);
     // let data_JSON = JSON.parse(data);
     // console.log(data_JSON);
 
+    // let data = localStorage.getItem(key);
+    // let data_JSON = JSON.parse(data);
+    // console.log(data_JSON);
 }
 // alert("Change");
 
@@ -87,7 +75,8 @@ $("#move").on("click",function(){
     window.location.href = "storage.html";
     //上でJSONに変換したものをstorage.htmlのローカルストレージに持っていきたい
 });
-alert("move");
+// alert("move");
 
 // 画面遷移時にオブジェクトにしたローカルストレージ内のデータを一覧ページに渡す関数
+//→同じ階層？だからかローカルストレージのデータが共有されている
 
--- js/storage.js
+++ js/storage.js
@@ -1,43 +1,60 @@
-// やりたいこと
-// ローカルストレージにデータがないときは「no data」的な文を表示する
-// すでに保存されている鍵と同じ名前で保存しようとしたらエラーが表示される
-// 消したいものを選んで消せるようにする
-// 消す前に「本当に削除しますか？」のダイアログ？ポップアップ？が出るようにする
-// 入力フォームに文字がないときはセーブを押せないようにする/両方に何かしらの入力があるときセーブが押せるようにする
+// 入力ページのローカルストレージデータをオブジェクトにして、JSONに変換して、セーブデータ一覧ページでgetJSONで持ってこれないか(GLS JS-03-03 getJSON関数より)→そんなことしなくてもローカルストレージが共有されていた
 
-// 入力ページのローカルストレージデータをオブジェクトにして、JSONに変換して、セーブデータ一覧ページでgetJSONで持ってこれないか(GLS JS-03-03 getJSON関数より)
+//ページ読み込み：保存データ取得表示
+// 繰り返し処理と配列を組み合わせて、ページのリロードをしたときに登録されているデータがあったら表示する
 
-
-// インプットページのセーブデータを持ってくる関数
-
-
-//3.ページ読み込み：保存データ取得表示
-// ページのリロードをしたときにローカルストレージにデータがあったら表示する
-// 繰り返し処理と配列を組み合わせて、登録されているデータがあったら表示する
-for(let i=0; i < localStorage.length; i++) {
-    const title = localStorage.title(i);
-    const value = localStorage.getItem(title);
-    // 下はテンプレートリテラル
+for(let i=0; i < localStorage.length; i++) { //←エラー「Assignment to constant variable」が出ていた  ←constをletにしたら直った。constは再代入、再宣言ができないのにforで何度も代入、宣言していたからエラーが出ていた？？
+    let key = localStorage.key(i); 
+    let value = localStorage.getItem(key);
+    // テンプレートリテラル
     // 斜めの「`」はShift + ＠
-        const html = `
+        let html = `
         <tr> 
-            <th>${title}</th>
-            <td>${value}</td>
+        <td><input type="checkbox" id="#check"/></td>
+        <th>${key}</th>
+        <td>${value}</td>
         </tr>
         `
         // 画面に表示
         $("#list").append(html);
         // #listの先頭に変数htmlを追加
-}
+        
+    }
 
 
-//2.clear クリックイベント
+//clear クリックイベント
 $("#clear").on("click",function(){
+    // 削除前に確認分を入れる
+    if(!confirm('全てのデータを削除します。よろしいですか？')){
+        return false;
+    }else{
+        localStorage.clear();
+        // ローカルストレージを空にする
+        $("#list").empty();
+        // listに表示されているhtml(メモパッドの下)を削除する
+    }
     // alert("clear");
-    localStorage.clear();
-    // ローカルストレージを空にする
-    $("#list").empty();
-    // listに表示されているhtml(メモパッドの下)を削除する
-    // →ローカルストレージに保存されているものを消すこととhtmlに表示されているものを消すことは別物
+});
+
+
+//クリックイベント 選択したもののみ削除→未完成
+$(function(){
+    $("#delete").prop("disabled", true); // 初期状態のボタンは無効
+    $("input[type='checkbox']").on('change', function () {
+        // チェックされているチェックボックスの数
+        if ($(".chk:checked").length > 0) {
+            // ボタン有効
+            $("#delete").prop("disabled", false);
+        } else {
+            // ボタン無効
+            $("#delete").prop("disabled", true);
+        }
+        // alert("delete");
+    });
+});
+
+//入力画面に戻る
+$("#back").on("click",function(){
+    window.location.href = "input.html";
 });
 
