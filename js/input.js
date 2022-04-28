3a3959ba2cc97ef34e5625da42156e694575971b
kdszk87 <kodai.suzuki.cd@gmail.com>
2022-04-29 01:47:18 +0900

    Firebase追加

--- js/input.js
+++ js/input.js
@@ -1,43 +1,21 @@
-// やりたいこと
-// 1 ローカルストレージにデータがないときは「no data」的な文を表示する
-// 2 すでに保存されている鍵と同じ名前で保存しようとしたらエラーが表示される
-// 3 消したいものを選んで消せるようにする
-// 4 消す前に「本当に削除しますか？」のダイアログ？ポップアップ？が出るようにする→OK
-// 5 入力フォームに文字がないときはセーブを押せないようにする/両方に何かしらの入力があるときセーブが押せるようにする→半分OK
-// 6 入力ページのローカルストレージデータをオブジェクトにして、JSONに変換して、セーブデータ一覧ページでgetJSONで持ってこれないか(GLS JS-03-03 getJSON関数より)
-
-
 //1.Save クリックイベント
 //両方のフォームに文字があるときのみ押せるようにする
 $(function(){
     $("#save").prop("disabled", true); // 初期状態のボタンは無効
     $("#key").on("input", function() {
-        const input = $("#key").val(); //input に入力された文字を取得
+        const input = $("#key").val();
         // console.log(input);
         if(input){ //もし文字が入っていれば
             $("#save").prop('disabled', false); 
-            //disabled を無効にする＝ボタンが押せる
         }else{
             $("#save").prop('disabled', true); 
-            //disabled を有効にする＝ボタンが押せない
         }
     });
 });
 
 $("#save").on("click", function (){
-
     let label = `<p>Saved the followed Record</p>`
     $("#explain").html(label);
-    //appendだと見出しもどんどん増えるので、ここはhtmlで。
-
-    let key = $("#key").val();
-    // valでkeyに入力された中身を持ってきている
-    console.log(key);
-    let flow = $("#flow").val();
-    // valでflowに入力された中身を持ってきている
-    console.log(flow);
-    // ↓local storageという保存場所にデータを保存する
-    localStorage.setItem(key, flow);
 
     let html = `
     <tr>
@@ -47,28 +25,13 @@ $("#save").on("click", function (){
     </tr>
     `
     // #list内に変数html内の情報を表示
-    $("#list").append(html);
+    $("#list").html(html);
 
     //Saveしたら入力フォームをクリアする
     $('input').val("");
     $('textarea').val("");
 
-})
-    
-
-// ローカルストレージ内のデータをオブジェクトにしたい
-    // for(let i=0; i < localStorage.length; i++) {
-    // let data = localStorage.key(i);
-   
-    //オブジェクトにしたものをJSONに変換したい
-    // let data_JSON = JSON.parse(data);
-    // console.log(data_JSON);
-
-    // let data = localStorage.getItem(key);
-    // let data_JSON = JSON.parse(data);
-    // console.log(data_JSON);
-// }
-// alert("Change");
+});
 
 
 // 画面遷移
@@ -78,6 +41,4 @@ $("#move").on("click",function(){
 });
 // alert("move");
 
-// 画面遷移時にオブジェクトにしたローカルストレージ内のデータを一覧ページに渡す関数
-//→同じ階層？だからかローカルストレージのデータが共有されている
 
--- js/storage.js
+++ js/storage.js
@@ -1,26 +1,3 @@
-// 入力ページのローカルストレージデータをオブジェクトにして、JSONに変換して、セーブデータ一覧ページでgetJSONで持ってこれないか(GLS JS-03-03 getJSON関数より)→そんなことしなくてもローカルストレージが共有されていた
-
-//ページ読み込み：保存データ取得表示
-// 繰り返し処理と配列を組み合わせて、ページのリロードをしたときに登録されているデータがあったら表示する
-
-for(let i=0; i < localStorage.length; i++) { //←エラー「Assignment to constant variable」が出ていた  ←constをletにしたら直った。constは再代入、再宣言ができないのにforで何度も代入、宣言していたからエラーが出ていた？？
-    let key = localStorage.key(i); 
-    let value = localStorage.getItem(key);
-    // テンプレートリテラル
-    // 斜めの「`」はShift + ＠
-        let html = `
-        <tr> 
-        <td><input type="checkbox" id="#check"/></td>
-        <th>${key}</th>
-        <td>${value}</td>
-        </tr>
-        `
-        // 画面に表示
-        $("#list").append(html);
-        // #listの先頭に変数htmlを追加
-    }
-
-
 //clear クリックイベント
 $("#clear").on("click",function(){
     // 削除前に確認分を入れる
@@ -28,9 +5,7 @@ $("#clear").on("click",function(){
         return false;
     }else{
         localStorage.clear();
-        // ローカルストレージを空にする
         $("#list").empty();
-        // listに表示されているhtml(メモパッドの下)を削除する
     }
     // alert("clear");
 });
@@ -48,7 +23,7 @@ $(function(){
     });
 });
 
-$("#delete").prop("disabled", true); // 初期状態のボタンは無効
+$("#delete").prop("disabled", true);
 $("input[type='checkbox']").on('change', function () {
     $('input[name="#check"]').prop("checked",true);
     const count = $("#check input:checkbox:checked").length;
@@ -66,6 +41,6 @@ $("input[type='checkbox']").on('change', function () {
 
 //入力画面に戻る
 $("#back").on("click",function(){
-    window.location.href = "input.html";
+    window.location.href = "index.html";
 });
 
